046616d5edeeb908488a620cc6a9e1ee193039ec
Alpimax <alperrozkann@gmail.com>
2022-08-26 13:31:57 -0400

    I finished my code.

--- src/Coding_Bad_day_Questions/String2/RepeatEnd.java
+++ src/Coding_Bad_day_Questions/String2/RepeatEnd.java
@@ -16,17 +16,22 @@ public class RepeatEnd {
 //    I'll go with for loop, but my loop have to turn my integer input number.And then, I'll use substring method for get result.
 
     public static void main(String[] args) {
-        System.out.println(method("Hello",3));
+        System.out.println(method("Hello", 3));
     }
-    public static String method(String str, int number){
-        String result="";
-        for (int i = 1; i <=number; i++) {
-            result+= str.substring(str.length()-number);
+
+    public static String method(String str, int number) {
+        String result = "";
+        if (number > 0 && number < str.length()) {
+            // I created my logic and checked, it worked, but I read question and found bug.
+            //   Which was question give us different test case
+            //You may assume that n is between 0 and the length of the string, inclusive.
+            //    I decided, put all code inside if statement.
+            for (int i = 1; i <= number; i++) {
+                result += str.substring(str.length() - number);
+            }
+            return result;
         }
-        return result;
+        return str;
     }
-    // I created my logic and checked, it worked, but I read question and found bug.
-//You may assume that n is between 0 and the length of the string, inclusive.
-//    I decided, put all code inside if statement.
 
 }
