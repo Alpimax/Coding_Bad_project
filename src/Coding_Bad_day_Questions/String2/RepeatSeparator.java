93d25d6f323dec8e3060ad93782b9a7d85fe1ad6
Emre YILMAZ <107490353+emcemen@users.noreply.github.com>
2022-08-26 02:45:22 -0400

    I created question 6 day_1

---
+++ src/Coding_Bad_day_Questions/String2/RepeatSeparator.java
@@ -0,0 +1,16 @@
+package Coding_Bad_day_Questions.String2;
+
+public class RepeatSeparator {
+    /*
+    Given two strings, word and a separator sep,
+    return a big string made of count occurrences of the word, separated by the separator string.
+
+
+    repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+    repeatSeparator("This", "And", 2) → "ThisAndThis"
+    repeatSeparator("This", "And", 1) → "This"
+
+     */
+
+    //YOUR CODE HERE
+}
