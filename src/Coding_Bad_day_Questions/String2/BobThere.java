dd52efbda029316591289a651d205a6d43d21b42
Emre YILMAZ <107490353+emcemen@users.noreply.github.com>
2022-08-26 13:24:54 -0400

    I created question 8 day_2

---
+++ src/Coding_Bad_day_Questions/String2/BobThere.java
@@ -0,0 +1,12 @@
+package Coding_Bad_day_Questions.String2;
+
+public class BobThere {
+    /*
+    Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+
+bobThere("abcbob") → true
+bobThere("b9b") → true
+bobThere("bac") → false
+     */
+}
