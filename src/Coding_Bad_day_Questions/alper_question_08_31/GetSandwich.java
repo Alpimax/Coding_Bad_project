43e33aae13b1a13aa32f77192854f4d84cfd4703
Emre YILMAZ <107490353+emcemen@users.noreply.github.com>
2022-09-01 10:32:15 -0400

    I created your daily questions

---
+++ src/Coding_Bad_day_Questions/alper_question_08_31/GetSandwich.java
@@ -0,0 +1,24 @@
+package Coding_Bad_day_Questions.alper_question_08_31;
+
+public class GetSandwich {
+
+    /*A sandwich is two pieces of bread with something in between. Return the string that is between the first and
+    last appearance of "bread" in the given string, or return the empty string "" if there are not two pieces of bread.
+
+
+
+            getSandwich("breadjambread") → "jam"
+    getSandwich("xxbreadjambreadyy") → "jam"
+    getSandwich("xxbreadyy") → ""
+*/
+    public static void main(String[] args) {
+        System.out.println(getSandwich("breadjambread"));
+    }
+
+    public static String getSandwich(String str) {
+
+       // here is your code
+
+
+    }
+}
---
+++ src/Coding_Bad_day_Questions/alper_question_09_01/ZipZap.java
@@ -0,0 +1,26 @@
+package Coding_Bad_day_Questions.alper_question_09_01;
+
+public class ZipZap {
+    /*
+    Look for patterns like "zip" and "zap" in the string -- length-3, starting with 'z' and ending with 'p'.
+     Return a string where for all such words, the middle letter is gone, so "zipXzap" yields "zpXzp".
+
+
+    zipZap("zipXzap") → "zpXzp"
+    zipZap("zopzop") → "zpzp"
+    zipZap("zzzopzop") → "zzzpzp"
+
+     */
+
+
+    public static void main(String[] args) {
+        System.out.println(zipZap("zipXzap"));
+    }
+    public static String zipZap(String str) {
+
+        //type your code here
+
+
+
+    }
+}
