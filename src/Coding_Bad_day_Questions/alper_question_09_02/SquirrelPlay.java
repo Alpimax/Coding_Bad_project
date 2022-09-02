946a916d399a262654242b3178102534d04f5872
Emre YILMAZ <107490353+emcemen@users.noreply.github.com>
2022-09-02 13:20:38 -0400

    I created your daily questions

---
+++ src/Coding_Bad_day_Questions/alper_question_09_02/SquirrelPlay.java
@@ -0,0 +1,24 @@
+package Coding_Bad_day_Questions.alper_question_09_02;
+
+public class SquirrelPlay {
+    /*
+    The squirrels in Palo Alto spend most of the day playing. In particular,
+    they play if the temperature is between 60 and 90 (inclusive). Unless it is summer,
+    then the upper limit is 100 instead of 90. Given an int temperature and a boolean isSummer,
+    return true if the squirrels play and false otherwise.
+
+    squirrelPlay(70, false) → true
+    squirrelPlay(95, false) → false
+     squirrelPlay(95, true) → true
+     */
+    public static void main(String[] args) {
+        System.out.println(squirrelPlay(70,false));
+    }
+    public static boolean squirrelPlay(int temp, boolean isSummer) {
+
+        //your code here
+
+
+    }
+
+}
