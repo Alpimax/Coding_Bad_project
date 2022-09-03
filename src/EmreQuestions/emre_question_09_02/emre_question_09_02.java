8e12096921228b3bd58bb1f2c3cda848be704681
Alpimax <alperrozkann@gmail.com>
2022-09-02 23:39:30 -0400

    questions are  ready

--- src/EmreQuestions/emre_question_09_02/emre_question_09_02.java
+++ src/EmreQuestions/emre_question_09_02/emre_question_09_02.java
@@ -1,4 +1,12 @@
 package EmreQuestions.emre_question_09_02;
 
 public class emre_question_09_02 {
+//    Array-2 > notAlone
+//prev  |  next  |  chance
+//We'll say that an element in an array is "alone" if there are values before and after it, and those values are different from it. Return a version of the given array where every instance of the given value which is alone is replaced by whichever value to its left or right is larger.
+//
+//
+//notAlone([1, 2, 3], 2) → [1, 3, 3]
+//notAlone([1, 2, 3, 2, 5, 2], 2) → [1, 3, 3, 5, 5, 2]
+//notAlone([3, 4], 3) → [3, 4]
 }
