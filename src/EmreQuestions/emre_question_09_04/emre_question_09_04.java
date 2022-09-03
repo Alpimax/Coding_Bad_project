2fdb17307b27459eb35f02b7b990eed7a186a295
Alpimax <alperrozkann@gmail.com>
2022-09-02 23:40:08 -0400

    questions are  ready

--- src/EmreQuestions/emre_question_09_04/emre_question_09_04.java
+++ src/EmreQuestions/emre_question_09_04/emre_question_09_04.java
@@ -1,4 +1,12 @@
 package EmreQuestions.emre_question_09_04;
 
 public class emre_question_09_04 {
+//    String-3 > mirrorEnds
+//prev  |  next  |  chance
+//Given a string, look for a mirror image (backwards) string at both the beginning and end of the given string. In other words, zero or more characters at the very begining of the given string, and at the very end of the string in reverse order (possibly overlapping). For example, the string "abXYZba" has the mirror end "ab".
+//
+//
+//mirrorEnds("abXYZba") → "ab"
+//mirrorEnds("abca") → "a"
+//mirrorEnds("aba") → "aba"
 }
